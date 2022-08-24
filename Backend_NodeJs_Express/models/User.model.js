a810275cdd2fa0edad8e80ff92dc1365e9445e14
jruiz-2019069 <jruiz-2019069@kinal.edu.gt>
2022-08-24 14:58:48 -0600

    Se realizaroón algunos cambios en la conexión a la base de datos.

---
+++ Backend_NodeJs_Express/models/User.model.js
@@ -0,0 +1,25 @@
+const sequelize = require("sequelize");
+
+const User = sequelize.define('User',{
+    id:{type: Sequelize.INTEGER, primaryKey: true},
+    username: Sequelize.STRING (255),
+    firstName: Sequelize.STRING,
+    lastName: Sequelize.STRING,
+    mail: Sequelize.STRING,
+    password: Sequelize.TEXT,
+    image: Sequelize.STRING,
+    job: Sequelize.STRING,
+    activated: Sequelize.TINYINT,
+    loginAttemps: Sequelize.INTEGER,
+    isLocked : Sequelize.TINYINT,
+    lockUntil: Sequelize.BIGINT,
+    resetPasswordToken: Sequelize.TEXT,
+    activateUserToken: Sequelize.TEXT,
+    sessionUserToken: Sequelize.TEXT,
+    deleted: Sequelize.TINYINT,
+    needChangePassword: Sequelize.TINYINT,
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE,
+})
+
+module.exports = User;
